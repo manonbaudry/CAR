612bde7f2a70d69fef6617b384bda98372316956
Brice D <58553835+BriceDspl@users.noreply.github.com>
2021-01-14 12:32:08 +0100

    commit try

--- TP1/src/main/FtpServerThread.java
+++ TP1/src/main/FtpServerThread.java
@@ -1,10 +1,12 @@
 package main;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 
 
@@ -15,7 +17,7 @@ import java.io.OutputStreamWriter;
  */
 public class FtpServerThread extends Thread {
 	private Socket cliSocket;
-	private boolean cliThreadRunning;
+	private boolean cliThreadRunning = true;
 	/**
 	 * Construct a thread by default
 	 */
@@ -29,29 +31,48 @@ public class FtpServerThread extends Thread {
 	 */
 	public FtpServerThread(Socket client) {
 		cliSocket = client;
-		cliThreadRunning = true;
 	}
 
 	/**
 	 * Run the exe of a thread.
 	 */
 	public void run() {
+		DataOutputStream controlOutWriter;
+		BufferedReader controlIn;
+		
 		System.out.println("Accepted Client Address - " + cliSocket.getInetAddress().getHostName());
-		BufferedReader in = null;
-		PrintWriter out = null;
+		
 		try {
-			System.out.println("test");
-			//Create in and out to communicate with client
-			in = new BufferedReader(new InputStreamReader(cliSocket.getInputStream()));
-			out = new PrintWriter(new OutputStreamWriter(cliSocket.getOutputStream()));			
-
-			out.println("220 Service ready\r\n");
-			while(cliThreadRunning) {
-				String cliCommand = in.readLine();
-				System.out.println(cliCommand);
-			}
+			   // Input from client
+	            controlIn = new BufferedReader(new InputStreamReader(cliSocket.getInputStream()));
+	            
+	            // Output to client, automatically flushed after each print
+	            controlOutWriter = new DataOutputStream(cliSocket.getOutputStream());
+	            
+	            // Greeting
+	            controlOutWriter.write("220 service ready\r\n200 OPTS command UTF8 ON ok.\r\n".getBytes());
+	            controlOutWriter.flush();
+	            
+	            
+//	            // Get new command from client
+	            while (true)
+	            {
+	            	String t = controlIn.readLine();
+		            System.out.println(t);
+	            }
 			
 			
+			
+//			//Create in and out to communicate with client
+//			in = new BufferedReader(new InputStreamReader(cliSocket.getInputStream()));
+//			out = cliSocket.getOutputStream();			
+//			out.write("220 service ready\r\n".getBytes());
+//			out.flush();
+//			while(cliThreadRunning) {
+//				String cliCommand = in.readLine();
+//				System.out.println(cliCommand + " test");
+//			}
+//			
 		} catch (Exception e) {
 			System.out.println(e);
 		}
