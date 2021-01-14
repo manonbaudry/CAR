48933fc068c8c730fc27b7d1fc8ee18c3ed433cb
manonbaudry <man.baudry@gmail.com>
2021-01-14 11:22:19 +0100

    remove unused class

--- TP1/src/main/Main.java
+++ TP1/src/main/Main.java
@@ -1,24 +1,12 @@
 package main;
 
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.util.HashMap;
-
 /***
- * @author Manon Baudry
+ * @author Manon Baudry Brice Despelchin
  */
 public class Main {
 
     public static void main(String[] args) {
        new FtpServer();
-//
-//        try{
-//            //ServerSocket serverSocket = new ServerSocket();
-//
-//        }catch (IOException e){
-//            e.printStackTrace();
-//        }
-
     }
 
 }
