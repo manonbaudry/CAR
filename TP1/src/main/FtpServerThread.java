03393a41ed170b844fc47d41031b1f9b6896ca85
manonbaudry <man.baudry@gmail.com>
2021-01-21 08:53:42 +0100

    Authentication

--- TP1/src/main/FtpServerThread.java
+++ TP1/src/main/FtpServerThread.java
@@ -2,12 +2,8 @@ package main;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.net.Socket;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 
 
 /**
@@ -24,7 +20,7 @@ public class FtpServerThread extends Thread {
 	public FtpServerThread() {
 		super();
 	}
-	
+
 	/**
 	 * Initiate the client to our class
 	 * @param client - the client to map
@@ -39,40 +35,36 @@ public class FtpServerThread extends Thread {
 	public void run() {
 		DataOutputStream controlOutWriter;
 		BufferedReader controlIn;
-		
+
 		System.out.println("Accepted Client Address - " + cliSocket.getInetAddress().getHostName());
-		
+
 		try {
-			   // Input from client
-	            controlIn = new BufferedReader(new InputStreamReader(cliSocket.getInputStream()));
-	            
-	            // Output to client, automatically flushed after each print
-	            controlOutWriter = new DataOutputStream(cliSocket.getOutputStream());
-	            
-	            // Greeting
-	            controlOutWriter.write("220 service ready\r\n200 OPTS command UTF8 ON ok.\r\n".getBytes());
-	            controlOutWriter.flush();
-	            
-	            
-//	            // Get new command from client
-	            while (true)
-	            {
-	            	String t = controlIn.readLine();
-		            System.out.println(t);
-	            }
-			
-			
-			
-//			//Create in and out to communicate with client
-//			in = new BufferedReader(new InputStreamReader(cliSocket.getInputStream()));
-//			out = cliSocket.getOutputStream();			
-//			out.write("220 service ready\r\n".getBytes());
-//			out.flush();
-//			while(cliThreadRunning) {
-//				String cliCommand = in.readLine();
-//				System.out.println(cliCommand + " test");
-//			}
-//			
+			// Input from client
+			controlIn = new BufferedReader(new InputStreamReader(cliSocket.getInputStream()));
+
+			// Output to client, automatically flushed after each print
+			controlOutWriter = new DataOutputStream(cliSocket.getOutputStream());
+
+			// Greeting
+			controlOutWriter.writeBytes("220 service ready\r\n");
+			controlOutWriter.flush();
+
+
+			// Authenticates the user
+			String userName = controlIn.readLine();
+			System.out.println(userName);
+			if(!userName.equals("USER toto"))
+				throw new Exception("Wrong username");
+
+			controlOutWriter.writeBytes("331 User name ok, need password\r\n");
+			String pwd = controlIn.readLine();
+			System.out.println(pwd);
+
+			if(!pwd.equals("PASS toto"))
+				throw new Exception("Wrong password");
+
+			controlOutWriter.writeBytes("230 User logged in\r\n");
+
 		} catch (Exception e) {
 			System.out.println(e);
 		}
