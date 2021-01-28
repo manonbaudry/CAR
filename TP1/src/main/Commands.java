9eaa4755be97cac13bbe99fa83b8684265a55804
manonbaudry <man.baudry@gmail.com>
2021-01-28 08:16:12 +0100

    add messages enum

--- TP1/src/main/Commands.java
+++
@@ -1,5 +0,0 @@
-package main;
-
-public enum Commands {
-    user, pass, dir, get, put, cd, quit
-}
---
+++ TP1/src/main/Messages.java
@@ -0,0 +1,27 @@
+package main;
+
+public class Messages {
+    public static final String MSG_220 = "220 service ready";
+    public static final String MSG_425 = "425 Can't open data connection.";
+    public static final String MSG_501 = "501 Syntax error in parameters or arguments.";
+    public static final String MSG_501_NO_FILENAME = "501 No filename given";
+    public static final String MSG_250 = "250 Requested file action okay, completed.";
+
+    public static final String MSG_550_UNAVAILABLE = "550 Requested action not taken. File unavailable";
+    public static final String MSG_550_NOT  = "550 File does not exist";
+    public static final String MSG_550_ALREADY  = "550 File already exists";
+
+    public static final String MSG_150 = "150 Opening ASCII mode data connection for requested file";
+    public static final String MSG_226_CLOSE = "226 Closing data connection. Requested file action successful.";
+    public static final String MSG_226_COMPLETE = "226 Transfer complete";
+    public static final String MSG_125 = "125 Opening ASCII mode data connection for file list";
+
+    public static final String MSG_530_NOT = "530 Not logged in";
+    public static final String MSG_530_ALREADY = "530 User already logged in";
+
+    public static final String MSG_331 = "331 User name ok, need password";
+    public static final String MSG_230 = "230 User logged in";
+    public static final String MSG_221 = "221 Closing connection";
+
+
+}
