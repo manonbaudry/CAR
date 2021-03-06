0acbff347750b6017196e519570c5b778140ac65
manonbaudry <man.baudry@gmail.com>
2021-03-06 21:51:25 +0100

    Get product in stock

--- ecommerce/src/main/java/com/tp2/ecommerce/repositories/StockRepository.java
+++ ecommerce/src/main/java/com/tp2/ecommerce/repositories/StockRepository.java
@@ -4,6 +4,10 @@ import com.tp2.ecommerce.entities.Product;
 import com.tp2.ecommerce.entities.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StockRepository extends JpaRepository<Stock, Long> {
     Stock findByProduct(Product product);
+
+    List<Stock> findByRemainingProductsGreaterThan(int quantity);
 }
--- ecommerce/src/main/java/com/tp2/ecommerce/services/StockService.java
+++ ecommerce/src/main/java/com/tp2/ecommerce/services/StockService.java
@@ -32,6 +32,6 @@ public class StockService {
     }
 
     public List<Stock> getProductInStock() {
-        return null;
+        return stockRepository.findByRemainingProductsGreaterThan(0);
     }
 }
